54c208a7770d697906eb66a93bede438c9a7891d
Alexandre Sant`anna <78821541+AlexandrePachecoo@users.noreply.github.com>
2025-03-26 01:44:51 +0000

    Titulos e input de texto

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
 
 function Home() {
-    return <h1>Olá mundo, Estou na internet</h1>
-}
+    return (
+        <div>
+            <div>
+                <h2>Hello world, <br />
+                    I'm on the web!
+                </h2>
+                <h3>I am learning English, and I created this page to be a personal dictionary.</h3>
+            </div>
 
+            <div>
+                <p>Type here:</p>
+                <input type="text"></input>
+            </div>
+        </div>
+    );
+}
 export default Home;
